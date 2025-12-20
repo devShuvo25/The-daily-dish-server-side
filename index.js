f5ff3cf09e987826e85beaf82be35d57513214e2
Shuvo Mallik <shuvomallik999@gmail.com>
2025-12-20 13:07:26 +0600

    order filter functionality added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-require("dotenv").config();
-const { connectDB } = require('./config/database');
-const app = express();
-const {run }= require('./lib/apiResponse')
-const { stripeRoutes } = require('./stripe/stripe');
-app.use(cors())
-app.use(express.json());
-const port = 3000;
-connectDB();
-run(app).catch(console.dir);
-stripeRoutes(app);
-app.listen(port,() => {
-    console.log("Server is runnig on port:",port);;
-})
-
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const { connectDB } = require('./config/database');
+const app = express();
+const {run }= require('./lib/apiResponse')
+const { stripeRoutes } = require('./stripe/stripe');
+app.use(cors())
+app.use(express.json());
+const port = 3000;
+connectDB();
+run(app).catch(console.dir);
+stripeRoutes(app);
+app.listen(port,() => {
+    console.log("Server is runnig on port:",port);;
+})
+
 module.exports = app;
